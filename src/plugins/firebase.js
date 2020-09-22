1fbdc10b35e8dbdc6df570f212fb205b6e41641d
Yosuke Ishii <ishii0514@gmail.com>
2020-09-22 09:42:56 +0900

    時間表示修正

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -1,9 +1,12 @@
 import Vue from "vue";
-import { firestorePlugin } from "vuefire";
+//import { firestorePlugin } from "vuefire";
+import { rtdbPlugin } from "vuefire";
 import firebase from "firebase/app";
 import "firebase/firestore";
+import "firebase/database";
 
-Vue.use(firestorePlugin);
+//Vue.use(firestorePlugin);
+Vue.use(rtdbPlugin);
 
 const firebaseApp = firebase.initializeApp({
   apiKey: process.env.VUE_APP_API_KEY,
@@ -16,4 +19,21 @@ const firebaseApp = firebase.initializeApp({
   measurementId: process.env.VUE_APP_MEASUREMENT_ID,
 });
 
+const moistureProjectFirebaseConfig = {
+  apiKey: "AIzaSyDQZQbgni9ECcmkfIhdXTHqHislgFTqF6M",
+  authDomain: "moisture-6f216.firebaseapp.com",
+  databaseURL: "https://moisture-6f216.firebaseio.com",
+  projectId: "moisture-6f216",
+  storageBucket: "moisture-6f216.appspot.com",
+  messagingSenderId: "841098403922",
+  appId: "1:841098403922:web:5937541984f7295c393f8b",
+  measurementId: "G-DWG77GWVDW",
+};
+//他のプロジェクトへの接続
+const moisutureProject = firebase.initializeApp(
+  moistureProjectFirebaseConfig,
+  "moisture"
+);
+
+export const moisture_db = moisutureProject.database();
 export const db = firebaseApp.firestore();
--- src/views/RealtimeGraph.vue
+++ src/views/RealtimeGraph.vue
@@ -2,16 +2,13 @@
   <div>
     <h3>realtime graph</h3>
     <LineChart :chart-data="datacollection" :options="options" />
-
-    <li v-for="(temp, index) in temperatures" :key="index">
-      {{ temp.time.toDate().toLocaleString() }}: {{ temp.temperature }}
-    </li>
   </div>
 </template>
 
 <script>
-import { db } from "../plugins/firebase";
+import { moisture_db } from "../plugins/firebase";
 import LineChart from "./LineChart2.vue";
+import moment from "moment";
 export default {
   components: { LineChart },
   data() {
@@ -20,25 +17,63 @@ export default {
       options: {
         responsive: true,
         maintainAspectRatio: false,
+        scales: {
+          xAxes: [
+            {
+              type: "time",
+              distribution: "series",
+              //time: {
+              //  unit: "second",
+              //  //parser: "YYYY-MM-DD HH:mm:ss",
+              //},
+            },
+          ],
+          yAxes: [
+            {
+              id: "y-axis-1", // Y軸のID
+              type: "linear", // linear固定
+              position: "left", // どちら側に表示される軸か？
+            },
+            {
+              id: "y-axis-2",
+              type: "linear",
+              position: "right",
+            },
+          ],
+        },
       },
     };
   },
-  firestore() {
+  firebase() {
     return {
-      temperatures: db.collection("graph_data").orderBy("time"),
+      temperatures: moisture_db.ref("moisture"),
     };
   },
   computed: {
     datacollection() {
       return {
-        labels: this.temperatures.map((x) => x.time.toDate().toLocaleString()),
+        labels: this.temperatures.map((x) => this.date_conv(x.date)),
         datasets: [
           {
             label: "temperature",
-            data: this.temperatures.map((x) => x.temperature),
+            data: this.temperatures.map((x) => x.Temp),
+            yAxisID: "y-axis-1",
+            borderColor: "rgba(254,97,132,0.8)",
+          },
+          {
+            label: "humidity",
+            data: this.temperatures.map((x) => x.Humid),
+            yAxisID: "y-axis-2",
+            borderColor: "rgba(54,164,235,0.8)",
           },
         ],
       };
+    },
+  },
+  methods: {
+    date_conv(str) {
+      const arry = str.split(":");
+      return moment(arry[0] + " " + arry.slice(1).join(":"));
     },
   },
 };
